fef48ab1e7cad066cc4248305db961ca189e0380
Peter Chamley <13586812+PJChamley@users.noreply.github.com>
2018-04-24 11:51:43 +0100

    Change PrivateSettingsChecker to work with only 1 csproj file. Now returns array even when only 1 File Found.

--- Scripts/PrivateSettingsChecker.ps1
+++ Scripts/PrivateSettingsChecker.ps1
@@ -1,11 +1,11 @@
-Write-Host "Working Directory: $((Get-Item -Path '.\' -Verbose).FullName)"
+Write-Host "Working Directory: $((Get-Item -Path '.\' -Verbose).FullName)" -ForegroundColor Magenta
 
 # Regex to find references in .csproj file
 $privateSettings = "(?i)include=`"PrivateSettings`.config`"(?-i)"
 $privateConnectionStrings = "(?i)include=`"PrivateConnectionStrings`.config`"(?-i)"
 
 # Get .csproj files recursively
-$filesArr = Get-ChildItem -Filter *.csproj -Recurse -ErrorAction SilentlyContinue -Force
+$filesArr = @(Get-ChildItem -Filter *.csproj -Recurse -ErrorAction SilentlyContinue -Force)
 
 # Iterate each found .csproj file
 For ($i=0; $i -lt $filesArr.Length; $i++) {
@@ -15,33 +15,27 @@ For ($i=0; $i -lt $filesArr.Length; $i++) {
     $privateConnectionStringsFound = 0
 
     $line = Get-Content $filesArr[$i].FullName;
-        if ($line -cmatch $privateSettings)
-        {
-            $privateSettingsFound = 1
-        }
-        if ($line -cmatch $privateConnectionStrings) {
-            $privateConnectionStringsFound = 1
-        }
+    if ($line -cmatch $privateSettings)
+    {
+        $privateSettingsFound = 1
+    }
+    if ($line -cmatch $privateConnectionStrings) {
+        $privateConnectionStringsFound = 1
+    }
 
-       write-host "Private settings count: $privateSettingsFound"   -ForegroundColor Green
-     write-host "Private connection settings count: $privateConnectionStringsFound"   -ForegroundColor Green
+    write-host "Private settings count: $privateSettingsFound" -ForegroundColor Magenta
+    write-host "Private connection settings count: $privateConnectionStringsFound" -ForegroundColor Magenta
     # If we need to create a privatesettings.config file then check it doesn't exist, and if not create
-    if ($privateSettingsFound -eq 1) {
-    
+    if ($privateSettingsFound -eq 1) {    
         $fileToCreate = "$($filesArr[$i].DirectoryName)\PrivateSettings.config"
         if (-Not(Test-Path $fileToCreate)) {
             Write-Host "Creating: $fileToCreate"
             New-Item $fileToCreate -type file
         }
-    }
-        
-         
-
-  
+    }        
 
     # If we need to create a privateconnectionstrings.config file then check it doesn't exist, and if not create
     if ($privateConnectionStringsFound -eq 1) {
-  
         $fileToCreate = "$($filesArr[$i].DirectoryName)\PrivateConnectionStrings.config"
         if (-Not (Test-Path $fileToCreate)) {
             Write-Host "Creating: $fileToCreate"
@@ -49,4 +43,3 @@ For ($i=0; $i -lt $filesArr.Length; $i++) {
         }
     }    
 }
-
