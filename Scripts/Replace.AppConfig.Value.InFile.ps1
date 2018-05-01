824afe98e804edab0e9785aa079766bc4b4d43c8
Peter Chamley <13586812+PJChamley@users.noreply.github.com>
2018-05-01 15:10:54 +0100

    Add Powershell script that will repalce values in an xml file.

---
+++ Scripts/Replace.AppConfig.Value.InFile.ps1
@@ -0,0 +1,48 @@
+﻿param
+(
+    [Parameter(Mandatory = $true)]
+    [string]
+    $appConfigFile,
+
+    [Parameter(Mandatory = $true)]
+    [string]
+    $AppSettingsKeyName,
+
+    [Parameter(Mandatory = $true)]
+    [string]
+    $AppSettingsKeyValue,
+
+    [Parameter(Mandatory = $false)]
+    [bool]
+    $EnableDebug=$false
+)
+
+if (!(Test-Path -Path $appConfigFile))
+{
+    Write-Host "File Not Found : $($appConfigFile)" -ForegroundColor Red
+}
+else
+{
+    $doc = (Get-Content $appConfigFile) -as [Xml]
+    If (!$doc)
+    {
+        Write-Host "Xml Config empty !!";
+    }
+    else
+    {
+        $obj = ($doc.configuration.connectionStrings.add | where {$_.Name -eq "$($AppSettingsKeyName)"})
+
+        if (!$obj)
+        {
+            Write-Host "Connection String NOT Found";
+        }
+        else
+        {
+            if ( $EnableDebug) { write-host "Before Value : $($obj.connectionString)" }
+            $obj.connectionString = $AppSettingsKeyValue
+
+            if ( $EnableDebug) { write-host "after Value : $($obj.connectionString)" }
+            $doc.Save($appConfigFile)
+        }
+    }
+}
