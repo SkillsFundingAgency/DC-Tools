f1ca88813ec7b8c2c5bba664617e5fba0ca7ef9f
Peter Chamley <13586812+PJChamley@users.noreply.github.com>
2018-09-21 10:33:46 +0100

    Update Replace.AppConfig.Value.InFile.ps1
    Add section to check app settings as well as Connection Strings.

--- Scripts/Replace.AppConfig.Value.InFile.ps1
+++ Scripts/Replace.AppConfig.Value.InFile.ps1
@@ -1,21 +1,12 @@
-﻿param
+param
 (
-    [Parameter(Mandatory = $true)]
-    [string]
-    $appConfigFile,
-
-    [Parameter(Mandatory = $true)]
-    [string]
-    $AppSettingsKeyName,
-
-    [Parameter(Mandatory = $true)]
-    [string]
-    $AppSettingsKeyValue,
-
-    [Parameter(Mandatory = $false)]
-    [bool]
-    $EnableDebug=$false
+    [Parameter(Mandatory = $true)] [string] $appConfigFile,
+    [Parameter(Mandatory = $true)] [string] $AppSettingsKeyName,
+    [Parameter(Mandatory = $true)] [string] $AppSettingsKeyValue,
+    [Parameter(Mandatory = $false)] [bool] $EnableDebug=$false
 )
+
+[boolean] $Found = $false;
 
 if (!(Test-Path -Path $appConfigFile))
 {
@@ -30,11 +21,12 @@ else
     }
     else
     {
+        ## Check Connection String for Setting
         $obj = ($doc.configuration.connectionStrings.add | where {$_.Name -eq "$($AppSettingsKeyName)"})
 
         if (!$obj)
         {
-            Write-Host "Connection String NOT Found";
+           if ( $EnableDebug) { Write-host "NOT Found in Connection String "; }
         }
         else
         {
@@ -43,6 +35,30 @@ else
 
             if ( $EnableDebug) { write-host "after Value : $($obj.connectionString)" }
             $doc.Save($appConfigFile)
+            $Found = $true
+        }
+
+        ## Check App Settings for Setting
+        $obj = ($doc.configuration.appSettings.add | where {$_.Key -eq "$($AppSettingsKeyName)"})
+
+        if (!$obj)
+        {
+           if ( $EnableDebug) { Write-host "NOT Found in App Settings"; }
+        }
+        else
+        {
+            if ( $EnableDebug) { write-host "Before Value : $($obj.value)" }
+            $obj.value = $AppSettingsKeyValue
+
+            if ( $EnableDebug) { write-host "after Value : $($obj.value)" }
+            $doc.Save($appConfigFile)
+            $Found = $true
+        }
+
+        #Display if Not Found
+        if (!$Found)
+        {
+           Write-host "NOT Found : $($AppSettingsKeyName)"
         }
     }
 }
